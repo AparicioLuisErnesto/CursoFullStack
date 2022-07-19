be9b5eac8adb8d92924d6d1245041a12d6df13e5
Luis E. Aparicio <eluisaparicio@gmail.com>
2022-07-19 07:00:21 -0300

    Unicafe Completed Exercise

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,69 +1,95 @@
-const Header = (props) => (
-  <div>
-    <h1>{props.course.name}</h1>
-  </div>
-)
+import { useState } from 'react'
 
-const Content = (props) => {
-  let partsComponent = []
-  props.course.parts.forEach(part => {
-    partsComponent.push(<Part key={part.name} courseName={part.name} courseNumberOfExercises={part.exercises} />)
+
+const Button = (props) => {
+  const clickHandler = props.clickHandler
+  return (
+    <button onClick={clickHandler}>{props.buttonText}</button>
+  )
+}
+
+const Statistics = (props) => {
+  const renderStatistics = []
+  props.statistics.forEach((statistic) => {
+    renderStatistics.push(<Statistic key={statistic.name} name={statistic.name} value={statistic.value} />);
   })
-  return (
-    <div>
-      {partsComponent}
-    </div>
-  )
-}
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.courseName} {props.courseNumberOfExercises}</p>
-    </div>
-  )
-
-}
-
-const Total = (props) => {
-  let total = 0
-  props.course.parts.forEach(part => {
-      total += part.exercises
-  })  
-  return (
-    <div>
-      <p>Number of exercises {total}</p>
-    </div>
-  )
-}
-
-
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  if (props.clicks > 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <table>
+          {renderStatistics}
+        </table>
+      </div>
+    )
   }
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <h1>Statistics</h1>
+      <p>No feedback given</p>
     </div>
   )
 }
 
+const Statistic = (props) => (
+  <tr><td>{props.name}</td><td>{props.value}</td></tr>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const buttonGoodFeedback = () => {
+    setGood(good + 1)
+  }
+  const buttonNeutralFeedback = () => {
+    setNeutral(neutral + 1)
+  }
+  const buttonBadFeedback = () => {
+    setBad(bad + 1)
+  }
+  const all = good + neutral + bad
+  const average = (good > 0 || bad > 0) ? (good - bad) / all : 0
+  const positive = good > 0 ? good * 100 / all + "%" : (0)
+  const statistics = [
+    {
+      name: "Good",
+      value: good
+    },
+    {
+      name: "Neutral",
+      value: neutral
+    },
+    {
+      name: "Bad",
+      value: bad
+    },
+    {
+      name: "All",
+      value: all
+    },
+    {
+      name: "Average",
+      value: average
+    },
+    {
+      name: "Positive",
+      value: positive
+    }
+
+  ]
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button clickHandler={buttonGoodFeedback} buttonText="Good"></Button>
+      <Button clickHandler={buttonNeutralFeedback} buttonText="Neutral"></Button>
+      <Button clickHandler={buttonBadFeedback} buttonText="Bad"></Button>
+      <Statistics statistics={statistics} clicks={all} />
+    </div>
+  )
+}
 
 export default App
